728b0740dc3790454756c4706106a7f8b8302ecb
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-05 16:46:34 +0100

    adding exercises folder

---
+++ exercises/exercise_1.rb
@@ -0,0 +1,33 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+
+  students = []
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
